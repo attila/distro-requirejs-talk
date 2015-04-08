a05df55b4de580e4cf878c6bed73482f49263602
Attila Beregszaszi <attila@frontseed.com>
2015-04-08 15:38:56 +0100

    Generated by generator-bespoke

--- bower_components/prism/components/prism-aspnet.js
+++ bower_components/prism/components/prism-aspnet.js
@@ -1,15 +1,15 @@
 Prism.languages.aspnet = Prism.languages.extend('markup', {
 	'page-directive tag': {
-		pattern: /(<|&lt;)%\s*@.*%>/gi,
+		pattern: /<%\s*@.*%>/i,
 		inside: {
-			'page-directive tag': /&lt;%\s*@\s*(?:Assembly|Control|Implements|Import|Master|MasterType|OutputCache|Page|PreviousPageType|Reference|Register)?|%>/ig,
+			'page-directive tag': /<%\s*@\s*(?:Assembly|Control|Implements|Import|Master|MasterType|OutputCache|Page|PreviousPageType|Reference|Register)?|%>/i,
 			rest: Prism.languages.markup.tag.inside
 		}
 	},
 	'directive tag': {
-		pattern: /(<|&lt;)%.*%>/gi,
+		pattern: /<%.*%>/i,
 		inside: {
-			'directive tag': /(<|&lt;)%\s*?[$=%#:]{0,2}|%>/gi,
+			'directive tag': /<%\s*?[$=%#:]{0,2}|%>/i,
 			rest: Prism.languages.csharp
 		}
 	}
@@ -21,16 +21,16 @@ Prism.languages.insertBefore('inside', 'punctuation', {
 }, Prism.languages.aspnet.tag.inside["attr-value"]);
 
 Prism.languages.insertBefore('aspnet', 'comment', {
-	'asp comment': /&lt;%--[\w\W]*?--%>/g
+	'asp comment': /<%--[\w\W]*?--%>/
 });
 
 // script runat="server" contains csharp, not javascript
 Prism.languages.insertBefore('aspnet', Prism.languages.javascript ? 'script' : 'tag', {
 	'asp script': {
-		pattern: /(&lt;|<)script(?=.*runat=['"]?server['"]?)[\w\W]*?(>|&gt;)[\w\W]*?(&lt;|<)\/script(>|&gt;)/ig,
+		pattern: /<script(?=.*runat=['"]?server['"]?)[\w\W]*?>[\w\W]*?<\/script>/i,
 		inside: {
 			tag: {
-				pattern: /&lt;\/?script\s*(?:\s+[\w:-]+(?:=(?:("|')(\\?[\w\W])*?\1|\w+))?\s*)*\/?>/gi,
+				pattern: /<\/?script\s*(?:\s+[\w:-]+(?:=(?:("|')(\\?[\w\W])*?\1|\w+))?\s*)*\/?>/i,
 				inside: Prism.languages.aspnet.tag.inside
 			},
 			rest: Prism.languages.csharp || {}
@@ -40,10 +40,10 @@ Prism.languages.insertBefore('aspnet', Prism.languages.javascript ? 'script' : '
 
 // Hacks to fix eager tag matching finishing too early: <script src="<% Foo.Bar %>"> => <script src="<% Foo.Bar %>
 if ( Prism.languages.aspnet.style ) {
-	Prism.languages.aspnet.style.inside.tag.pattern = /&lt;\/?style\s*(?:\s+[\w:-]+(?:=(?:("|')(\\?[\w\W])*?\1|\w+))?\s*)*\/?>/gi;
+	Prism.languages.aspnet.style.inside.tag.pattern = /<\/?style\s*(?:\s+[\w:-]+(?:=(?:("|')(\\?[\w\W])*?\1|\w+))?\s*)*\/?>/i;
 	Prism.languages.aspnet.style.inside.tag.inside = Prism.languages.aspnet.tag.inside;
 }
 if ( Prism.languages.aspnet.script ) {
-	Prism.languages.aspnet.script.inside.tag.pattern = Prism.languages.aspnet['asp script'].inside.tag.pattern
+	Prism.languages.aspnet.script.inside.tag.pattern = Prism.languages.aspnet['asp script'].inside.tag.pattern;
 	Prism.languages.aspnet.script.inside.tag.inside = Prism.languages.aspnet.tag.inside;
 }
